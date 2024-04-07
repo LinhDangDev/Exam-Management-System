24d34077cc5f9feeec2ca79c14d9e27c33ce35b4
Linh Dang Dev <duylinh05030723@gmail.com>
2024-04-08 01:54:13 +0700

    Add File

--- Visual Code/GettingStarted/Server/Controllers/ExamController.cs
+++ Visual Code/GettingStarted/Server/Controllers/ExamController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GettingStarted.Server.BUS;
+using GettingStarted.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GettingStarted.Server.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class ExamController : Controller
     {
-        public IActionResult Index()
+        SinhVienService _sinhVienService;
+        public ExamController(SinhVienService sinhVienService)
         {
-            return View();
+            _sinhVienService = sinhVienService;
+        }
+
+        [HttpPost]
+        [Route("/GetThongTinSV")]
+        public ActionResult<SinhVien> GetThongTinSV([FromBody]long ma_sinh_vien)
+        {
+            return _sinhVienService.GetSinhVien_FromMaSoSV(ma_sinh_vien);
         }
     }
 }
