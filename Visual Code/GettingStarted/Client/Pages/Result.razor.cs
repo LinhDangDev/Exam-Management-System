423db90703c4e4b7cc8e18f9f61b0656b4f41879
Linh Dang Dev <duylinh05030723@gmail.com>
2024-04-12 21:19:45 +0700

    Add UI Result and Services Result

--- Visual Code/GettingStarted/Client/Pages/Result.razor.cs
+++ Visual Code/GettingStarted/Client/Pages/Result.razor.cs
@@ -1,6 +1,26 @@
-﻿namespace GettingStarted.Client.Pages;
+﻿using GettingStarted.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text;
+using GettingStarted.Client.DAL;
+using Microsoft.IdentityModel.Tokens;
 
-public class Result_razor
+namespace GettingStarted.Client.Pages;
+
+public partial class Result
 {
+    [Inject]
+    HttpClient httpClient { get; set; }
+    [Inject]
+    NavigationManager navManager { get; set; }
     
+    
+    
+    private void NavigateToLogin()
+    {
+        navManager.NavigateTo("/Login");
+    }
+    
+
 }
---
+++ Visual Code/GettingStarted/Server/Controllers/ResultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GettingStarted.Server.Controllers;
+
+public class ResultController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
