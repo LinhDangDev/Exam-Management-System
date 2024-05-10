64eca6402d57443e24399eb001c891af12db9798
LinhDangDev <duylinh05030723@gmail.com>
2024-05-10 09:59:31 +0700

    Update

---
+++ GettingStarted/GettingStarted/Server/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using GettingStarted.Server.Authentication;
+using GettingStarted.Server.BUS;
+using GettingStarted.Shared;
+using GettingStarted.Shared.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace GettingStarted.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly SinhVienService _sinhVienService;
+        public UserController(SinhVienService sinhVienService) 
+        {
+            _sinhVienService = sinhVienService;
+        }
+
+        [HttpPost("Verify")]
+        public ActionResult<UserSession> Verify([FromQuery]string ma_so_sinh_vien)
+        {
+            var JwtAuthencationManager = new JwtAuthenticationManager(_sinhVienService);
+            var userSession = JwtAuthencationManager.GenerateJwtToken(ma_so_sinh_vien);
+            if(userSession is null)
+            {
+                return Unauthorized();
+            }
+            else
+            {
+                UpdateLogin(userSession.NavigateSinhVien.MaSinhVien);
+                return userSession;
+            }
+        }
+        private void UpdateLogin(long ma_sinh_vien)
+        {
+            _sinhVienService.Login(ma_sinh_vien, DateTime.Now);
+        }
+        [HttpPost("UpdateLogout")]
+        public ActionResult UpdateLogout([FromQuery] long ma_sinh_vien)
+        {
+            _sinhVienService.Logout(ma_sinh_vien, DateTime.Now);
+            return Ok();
+        }
+    }
+}
