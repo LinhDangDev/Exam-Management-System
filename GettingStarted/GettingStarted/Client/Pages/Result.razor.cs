879ab06af75fd46b8405fd481158c800c4dab285
LinhDangDev <duylinh05030723@gmail.com>
2024-05-16 10:11:02 +0700

    Responsive 4 Page: Login, Info, Exam, Result

--- GettingStarted/GettingStarted/Client/Pages/Result.razor.cs
+++ GettingStarted/GettingStarted/Client/Pages/Result.razor.cs
@@ -12,143 +12,153 @@ namespace GettingStarted.Client.Pages;
 public partial class Result
 {
     [Inject]
-    HttpClient httpClient { get; set; }
+    HttpClient? httpClient { get; set; }
     [Inject]
-    ApplicationDataService myData { get; set; }
+    ApplicationDataService? myData { get; set; }
     [Inject]
-    AuthenticationStateProvider authenticationStateProvider { get; set; }
+    AuthenticationStateProvider? authenticationStateProvider { get; set; }
     [Inject]
-    NavigationManager navManager { get; set; }
+    NavigationManager? navManager { get; set; }
     [Inject]
-    IJSRuntime js { get; set; }
+    IJSRuntime? js { get; set; }
     private SinhVien? sinhVien { get; set; }
     private CaThi? caThi { get; set; }
     private ChiTietCaThi? chiTietCaThi { get; set; }
     private List<TblChiTietDeThiHoanVi>? chiTietDeThiHoanVis { get; set; }
     private List<int>? listDapAn { get; set; }
+    private List<bool>? ketQuaDapAn { get; set; }
     private double diem { get; set; }
     private int so_cau_dung { get; set; }
-    protected override async Task OnInitializedAsync()
+    private async Task checkPage()
     {
-        if (myData.ma_ca_thi == null || myData.ma_de_thi_hoan_vi == null)
+        if ((myData == null || myData.chiTietCaThi == null || myData.sinhVien == null) && js != null)
         {
             await js.InvokeVoidAsync("alert", "Cách hoạt động trang trang web không hợp lệ. Vui lòng quay lại");
-            navManager.NavigateTo("/info");
+            navManager?.NavigateTo("/info");
             return;
         }
-        await Start();
+        if (myData != null && myData.chiTietCaThi != null)
+        {
+            khoiTaoBanDau();
+            chiTietCaThi = myData.chiTietCaThi;
+            caThi = myData.chiTietCaThi.MaCaThiNavigation;
+            sinhVien = myData.sinhVien;
+            chiTietDeThiHoanVis = myData.chiTietDeThiHoanVis;
+            await Start();
+        }
+    }
+    protected override async Task OnInitializedAsync()
+    {
+        await checkPage();
         //xác thực người dùng
-        var customAuthStateProvider = (CustomAuthenticationStateProvider)authenticationStateProvider;
-        var token = await customAuthStateProvider.GetToken();
-        if (!string.IsNullOrWhiteSpace(token))
+        var customAuthStateProvider = (authenticationStateProvider != null) ? (CustomAuthenticationStateProvider)authenticationStateProvider : null;
+        var token = (customAuthStateProvider != null) ? await customAuthStateProvider.GetToken() : null;
+        if (!string.IsNullOrWhiteSpace(token) && httpClient != null)
         {
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
         }
         else
         {
-            navManager.NavigateTo("/");
+            navManager?.NavigateTo("/");
         }
         await base.OnInitializedAsync();
     }
-    private async Task getThongTinSV()
+
+    private void ListDapAn()
     {
-        var response = await httpClient.PostAsync($"api/Result/GetThongTinSinhVien?ma_sinh_vien={myData.ma_sinh_vien}", null);
-        if (response.IsSuccessStatusCode)
+        // lấy đáp án từ chi tiết đề thi hoán vị
+        if(chiTietDeThiHoanVis != null && listDapAn != null)
         {
-            var resultString = await response.Content.ReadAsStringAsync();
-            sinhVien = JsonSerializer.Deserialize<SinhVien>(resultString, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
-        }
-    }
-    private async Task getThongTinCaThi()
-    {
-        var response = await httpClient.PostAsync($"api/Result/GetThongTinCaThi?ma_ca_thi={myData.ma_ca_thi}", null);
-        if (response.IsSuccessStatusCode)
-        {
-            var resultString = await response.Content.ReadAsStringAsync();
-            caThi = JsonSerializer.Deserialize<CaThi>(resultString, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
-        }
-    }
-    private async Task getChiTietDeThiHoanVi()
-    {
-        // gọi lấy lại từ ExamController
-        var response = await httpClient.PostAsync($"api/Exam/HandleAllQuestionExams?ma_de_thi_hoan_vi={myData.ma_de_thi_hoan_vi}", null);
-        if (response.IsSuccessStatusCode)
-        {
-            var resultString = await response.Content.ReadAsStringAsync();
-            chiTietDeThiHoanVis = JsonSerializer.Deserialize<List<TblChiTietDeThiHoanVi>>(resultString, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
-        }
-    }
-    private async Task GetListDapAn()
-    {
-        var jsonString = JsonSerializer.Serialize(chiTietDeThiHoanVis);
-        var response = await httpClient.PostAsync("api/Result/GetListDapAn", new StringContent(jsonString, Encoding.UTF8, "application/json"));
-        if (response.IsSuccessStatusCode)
-        {
-            var resultString = await response.Content.ReadAsStringAsync();
-            listDapAn = JsonSerializer.Deserialize<List<int>>(resultString, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
-        }
-    }
-    private async Task getThongTinChiTietCaThi()
-    {
-        var response = await httpClient.PostAsync($"api/Result/GetChiTietCaThiSelectBy_SinhVien?ma_ca_thi={myData.ma_ca_thi}&ma_sinh_vien={myData.ma_sinh_vien}", null);
-        if (response.IsSuccessStatusCode)
-        {
-            var resultString = await response.Content.ReadAsStringAsync();
-            chiTietCaThi = JsonSerializer.Deserialize<ChiTietCaThi>(resultString, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            foreach (var item in chiTietDeThiHoanVis)
+            {
+                if (item.DapAn != null)
+                    listDapAn.Add((int)item.DapAn);
+                else
+                    listDapAn.Add(-1);
+            }
         }
     }
     private async Task HandleUpdateKetThuc()
     {
-        chiTietCaThi.ThoiGianKetThuc = DateTime.Now;
-        chiTietCaThi.Diem = diem;
-        chiTietCaThi.SoCauDung = so_cau_dung;
-        chiTietCaThi.TongSoCau = chiTietDeThiHoanVis.Count;
-        var jsonString = JsonSerializer.Serialize(chiTietCaThi);
-        await httpClient.PostAsync("api/Result/UpdateKetThuc", new StringContent(jsonString, Encoding.UTF8, "application/json"));
+        if(chiTietCaThi != null && httpClient != null && chiTietDeThiHoanVis != null)
+        {
+            chiTietCaThi.ThoiGianKetThuc = DateTime.Now;
+            chiTietCaThi.Diem = diem;
+            chiTietCaThi.SoCauDung = so_cau_dung;
+            chiTietCaThi.TongSoCau = chiTietDeThiHoanVis.Count;
+            var jsonString = JsonSerializer.Serialize(chiTietCaThi);
+            await httpClient.PostAsync("api/Result/UpdateKetThuc", new StringContent(jsonString, Encoding.UTF8, "application/json"));
+        }
     }
     private void tinhDiemSo()
     {
         diem = so_cau_dung = 0;
-        double diem_tung_cau = (10.0 / chiTietDeThiHoanVis.Count);
-        int length = myData.listDapAnKhoanh.Count;
+        double diem_tung_cau = 0;
+        if(chiTietDeThiHoanVis != null)
+            diem_tung_cau = (10.0 / chiTietDeThiHoanVis.Count);
+        int length = 0;
+        if(myData != null && myData.listDapAnKhoanh != null)
+            length = myData.listDapAnKhoanh.Count;
         for(int i = 0; i < length; i++)
         {
             //nếu trùng đáp án thì có điểm, còn không trùng thì không có
-            if (myData.listDapAnKhoanh[i] == listDapAn[i])
+            if (myData!= null && myData.listDapAnKhoanh != null && listDapAn != null && myData.listDapAnKhoanh[i] == listDapAn[i] && ketQuaDapAn != null)
             {
                 diem += diem_tung_cau;
                 so_cau_dung++;
+                ketQuaDapAn.Add(true);
+            }
+            else
+            {
+                if(ketQuaDapAn != null)
+                    ketQuaDapAn.Add(false);
             }
         }
-        diem = Math.Round(diem,1);
+        diem = quyDoiDiem(diem);
+    }
+    private double quyDoiDiem(double diem)
+    {
+        double so_phay = diem % 1;
+        if (so_phay > 0 && so_phay <= 0.25)
+            return Math.Floor(diem) + 0.3;
+        if (so_phay > 0.25 && so_phay <= 0.5)
+            return Math.Floor(diem) + 0.5;
+        if (so_phay > 0.5 && so_phay <= 0.75)
+            return Math.Floor(diem) + 0.8;
+        if(so_phay > 0.75)
+            return Math.Ceiling(diem);
+        return Math.Floor(diem);
     }
     private async Task onClickDangXuatAsync()
     {
-        bool result = await js.InvokeAsync<bool>("confirm", "Bạn muốn đăng xuất?");
+        bool result = (js != null) && await js.InvokeAsync<bool>("confirm", "Bạn có chắc chắn muốn đăng xuất?");
         if (result)
         {
             await UpdateLogout();
-            var customAuthStateProvider = (CustomAuthenticationStateProvider)authenticationStateProvider;
-            await customAuthStateProvider.UpdateAuthenticationState(null);
-            navManager.NavigateTo("/", true);
+            var customAuthStateProvider = (authenticationStateProvider!=null) ? (CustomAuthenticationStateProvider)authenticationStateProvider : null;
+            if(customAuthStateProvider != null)
+                await customAuthStateProvider.UpdateAuthenticationState(null);
+            navManager?.NavigateTo("/", true);
         }
     }
     private async Task UpdateLogout()
     {
-        await httpClient.PostAsync($"api/Login/UpdateLogout?ma_sinh_vien={myData.ma_sinh_vien}&last_log_out={DateTime.Now}", null);
+        if(httpClient != null && sinhVien != null)
+            await httpClient.PostAsync($"api/User/UpdateLogout?ma_sinh_vien={sinhVien.MaSinhVien}", null);
     }
-
-    private async Task Start()
+    private void khoiTaoBanDau()
     {
         sinhVien = new SinhVien();
         caThi = new CaThi();
         chiTietCaThi = new ChiTietCaThi();
         chiTietDeThiHoanVis = new List<TblChiTietDeThiHoanVi>();
-        await getThongTinSV();
-        await getThongTinCaThi();
-        await getChiTietDeThiHoanVi();
-        await GetListDapAn();
-        await getThongTinChiTietCaThi();
+        listDapAn = new List<int>();
+        ketQuaDapAn = new List<bool>();
+    }
+
+    private async Task Start()
+    {
+        ListDapAn();
         tinhDiemSo();
         await HandleUpdateKetThuc();
     }
